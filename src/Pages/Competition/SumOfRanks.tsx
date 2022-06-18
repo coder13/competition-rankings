ddf2df4291be2c2b902c2cd07ee5bede817d9985
Choover <choover11@gmail.com>
2022-06-18 11:58:28 -0700

    shows medals

--- src/Pages/Competition/SumOfRanks.tsx
+++ src/Pages/Competition/SumOfRanks.tsx
@@ -3,6 +3,16 @@ import { Box } from '@mui/system';
 import { Competition } from '@wca/helpers';
 import { useCallback, useMemo } from 'react';
 
+export const parseActivityCode = (activityCode: string) => {
+  const [, e, r, g, a] = activityCode.match(/(\w+)(?:-r(\d+))?(?:-g(\d+))?(?:-a(\d+))?/);
+  return {
+    eventId: e,
+    roundNumber: r && parseInt(r, 10),
+    groupNumber: g && parseInt(g, 10),
+    attemptNumber: a && parseInt(a, 10),
+  };
+};
+
 export default function SumOfRanks({ persons, events }: Competition) {
   const resultsForPerson = useCallback(
     (registrantId: number) =>
@@ -11,13 +21,14 @@ export default function SumOfRanks({ persons, events }: Competition) {
           event.rounds
             .map((round) => ({
               activityCode: round.id,
+              finalRound: parseActivityCode(round.id)?.roundNumber === event.rounds.length,
               ...(round.results.find((result) => result.personId === registrantId) ?? {
                 ranking: round.results.length,
               }),
             }))
             .map((result) => ({
               ...result,
-              eventId: 'eventId',
+              eventId: event.id,
             }))
             .reduce((xs, x) => xs.concat(x), [])
         )
@@ -38,10 +49,13 @@ export default function SumOfRanks({ persons, events }: Competition) {
         )
         .map((person) => {
           const allResults = resultsForPerson(person.registrantId);
+          const rankings = allResults.map((result) => result.ranking);
+          const finalRounds = allResults.filter((result) => !!result.finalRound);
 
           return {
             ...person,
-            sumOfRanks: allResults.map((result) => result.ranking).reduce((a, b) => a + b),
+            sumOfRanks: rankings.reduce((a, b) => a + b),
+            medals: finalRounds.filter((result) => result.ranking <= 3),
           };
         }),
     [events, persons, resultsForPerson]
@@ -56,6 +70,7 @@ export default function SumOfRanks({ persons, events }: Competition) {
             <TableCell>Position</TableCell>
             <TableCell>Name</TableCell>
             <TableCell>Sum of Ranks</TableCell>
+            <TableCell>Medals</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -66,6 +81,12 @@ export default function SumOfRanks({ persons, events }: Competition) {
                 <TableCell>{index + 1}</TableCell>
                 <TableCell>{person.name}</TableCell>
                 <TableCell>{person.sumOfRanks}</TableCell>
+                <TableCell>
+                  {person.medals.length > 0 &&
+                    `${person.medals.length} (${person.medals
+                      .map((medal) => medal.eventId)
+                      .join(', ')})`}
+                </TableCell>
               </TableRow>
             ))}
         </TableBody>
