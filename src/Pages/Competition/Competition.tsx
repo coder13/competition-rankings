24da7faaa64698d647cecfa574ba424fe7664647
Choover <choover11@gmail.com>
2022-10-12 15:33:26 -0700

    Fixed bug

--- src/Pages/Competition/Competition.tsx
+++ src/Pages/Competition/Competition.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetCompetitionByIdQuery } from 'Services';
 import PersonRankings from './PersonRankings';
@@ -5,6 +6,14 @@ import PersonRankings from './PersonRankings';
 export default function Competition() {
   const { competitionId } = useParams();
   const { data: competition, isLoading, error } = useGetCompetitionByIdQuery(competitionId);
+
+  useEffect(() => {
+    if (competition) {
+      document.title = `${competition.shortName} - Competition Rankings`;
+    } else {
+      document.title = 'Competition Rankings';
+    }
+  }, [competition]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -14,14 +23,6 @@ export default function Competition() {
     console.error(error);
     return <div>Error!</div>;
   }
-
-  useEffect(() => {
-    if (competition) {
-      document.title = `${competition.shortName} - Competition Rankings`;
-    } else {
-      document.title = 'Competition Rankings';
-    }
-  }, [competition]);
 
   return (
     <div>
